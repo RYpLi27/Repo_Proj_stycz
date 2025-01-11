8c898f9ddaa2c1dd85f4620ba0d4a3b5eba449de
Zbigniew "RYpLi" Janasek <zjanasek@edu.cdv.pl>
2025-01-11 12:46:02 +0100

    Dupa

---
+++ Assets/Scripts/P_Move.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class P_Move : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        Debug.Log("dupa");
+
+    }
+    private void OnDisable()
+    {
+        Debug.Log("dupa");
+    }
+    private void OnDestroy()
+    {
+        Debug.Log("dupa");
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+
+
+
+
+
+
+}
